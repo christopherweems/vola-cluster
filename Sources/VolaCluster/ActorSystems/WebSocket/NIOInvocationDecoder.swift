78cb2168ac591cc417b32930f65b5dbed82f97fb
Christopher Weems <hello@christopherweems.com>
2023-09-06 22:36:24 -0400

    [gardening]

--- Sources/VolaCluster/ActorSystems/WebSocket/NIOInvocationDecoder.swift
+++ Sources/VolaCluster/ActorSystems/WebSocket/NIOInvocationDecoder.swift
@@ -1,9 +1,9 @@
 /*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-Invocation encoder into a NIO byte buffer.
-*/
+ See LICENSE folder for this sample’s licensing information.
+ 
+ Abstract:
+ Invocation encoder into a NIO byte buffer.
+ */
 
 import Distributed
 import Foundation
@@ -23,26 +23,26 @@ public class NIOInvocationEncoder: DistributedTargetInvocationEncoder {
     public typealias SerializationRequirement = any Codable
     var genericSubs: [String] = []
     var argumentData: [Data] = []
-
+    
     public func recordGenericSubstitution<T>(_ type: T.Type) throws {
         if let name = _mangledTypeName(T.self) {
             genericSubs.append(name)
         }
     }
-
+    
     public func recordArgument<Value: Codable>(_ argument: RemoteCallArgument<Value>) throws {
         let data = try JSONEncoder().encode(argument.value)
         self.argumentData.append(data)
     }
-
+    
     public func recordReturnType<R: Codable>(_ type: R.Type) throws {
         // noop, no need to record it in this system
     }
-
+    
     public func recordErrorType<E: Error>(_ type: E.Type) throws {
         // noop, no need to record it in this system
     }
-
+    
     public func doneRecording() throws {
         // noop, nothing to do in this system
     }
--- Sources/VolaCluster/ActorSystems/WebSocket/NIOInvocationEncoder.swift
+++ Sources/VolaCluster/ActorSystems/WebSocket/NIOInvocationEncoder.swift
@@ -1,9 +1,9 @@
 /*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-Invocation decoder from a NIO byte buffer.
-*/
+ See LICENSE folder for this sample’s licensing information.
+ 
+ Abstract:
+ Invocation decoder from a NIO byte buffer.
+ */
 
 import Distributed
 import Foundation
@@ -21,32 +21,32 @@ import NIOFoundationCompat
 @available(watchOS 9.0, *)
 public class NIOInvocationDecoder: DistributedTargetInvocationDecoder {
     public typealias SerializationRequirement = any Codable
-
+    
     let decoder: JSONDecoder
     let envelope: RemoteWebSocketCallEnvelope
     var argumentsIterator: Array<Data>.Iterator
-
+    
     public init(system: WebSocketActorSystem, envelope: RemoteWebSocketCallEnvelope) {
         self.envelope = envelope
         self.argumentsIterator = envelope.args.makeIterator()
-
+        
         let decoder = JSONDecoder()
         decoder.userInfo[.actorSystemKey] = system
         self.decoder = decoder
     }
-
+    
     public func decodeGenericSubstitutions() throws -> [Any.Type] {
         return envelope.genericSubs.compactMap { name in
             return _typeByName(name)
         }
     }
-
+    
     public func decodeNextArgument<Argument: Codable>() throws -> Argument {
         guard let data = argumentsIterator.next() else {
             log("decode-argument", "none left")
             throw WebSocketActorSystemError.notEnoughArgumentsInEnvelope(expected: Argument.self)
         }
-
+        
         do {
             let value = try decoder.decode(Argument.self, from: data)
             log("decode-argument", "decoded: \(value)")
@@ -56,12 +56,13 @@ public class NIOInvocationDecoder: DistributedTargetInvocationDecoder {
             throw error
         }
     }
-
+    
     public func decodeErrorType() throws -> Any.Type? {
         nil // not encoded, ok
     }
-
+    
     public func decodeReturnType() throws -> Any.Type? {
         nil // not encoded, ok
     }
+    
 }
