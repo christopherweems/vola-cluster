d60b4f49a0e21bf4fff42ca8623785d3780b09e1
Christopher Weems <hello@christopherweems.com>
2023-10-09 00:19:59 -0400

    [gardening]

--- Sources/VolaCluster/ActorSystems/WebSocket/ChannelHandlers.swift
+++ Sources/VolaCluster/ActorSystems/WebSocket/ChannelHandlers.swift
@@ -42,6 +42,9 @@ internal final class HTTPInitialRequestHandler: ChannelInboundHandler, Removable
         
     }
     
+}
+
+extension HTTPInitialRequestHandler {
     public func channelActive(context: ChannelHandlerContext) {
         // We are connected. It's time to send the message to the server to initialize the upgrade dance.
         var headers = HTTPHeaders()
@@ -63,15 +66,16 @@ internal final class HTTPInitialRequestHandler: ChannelInboundHandler, Removable
     }
     
     public func channelRead(context: ChannelHandlerContext, data: NIOAny) {
-        
         let clientResponse = self.unwrapInboundIn(data)
         
         switch clientResponse {
         case .head(let responseHead):
             print("Received status: \(responseHead.status)")
+            
         case .body(let byteBuffer):
             let string = String(buffer: byteBuffer)
             print("Received: '\(string)' back from the server.")
+            
         case .end:
             print("Closing channel.")
             context.close(promise: nil)
@@ -103,6 +107,9 @@ internal final class WebSocketMessageOutboundHandler: ChannelOutboundHandler {
         
     }
     
+}
+
+extension WebSocketMessageOutboundHandler {
     public func handlerRemoved(context: ChannelHandlerContext) {
         // While we do this, we should also notify the system about any cleanups
         // it might need to do. E.g. if it has receptionist connections to the peer
@@ -181,14 +188,17 @@ internal final class HTTPHandler: ChannelInboundHandler, RemovableChannelHandler
         headers.add(name: "Content-Type", value: "text/html")
         headers.add(name: "Content-Length", value: String(self.responseBody.readableBytes))
         headers.add(name: "Connection", value: "close")
+        
         let responseHead = HTTPResponseHead(version: .init(major: 1, minor: 1),
                                             status: .ok,
                                             headers: headers)
+        
         context.write(self.wrapOutboundOut(.head(responseHead)), promise: nil)
         context.write(self.wrapOutboundOut(.body(.byteBuffer(self.responseBody))), promise: nil)
         context.write(self.wrapOutboundOut(.end(nil))).whenComplete { (_: Result<Void, Error>) in
             context.close(promise: nil)
         }
+        
         context.flush()
     }
     
@@ -196,13 +206,16 @@ internal final class HTTPHandler: ChannelInboundHandler, RemovableChannelHandler
         var headers = HTTPHeaders()
         headers.add(name: "Connection", value: "close")
         headers.add(name: "Content-Length", value: "0")
+        
         let head = HTTPResponseHead(version: .http1_1,
                                     status: .methodNotAllowed,
                                     headers: headers)
+        
         context.write(self.wrapOutboundOut(.head(head)), promise: nil)
         context.write(self.wrapOutboundOut(.end(nil))).whenComplete { (_: Result<Void, Error>) in
             context.close(promise: nil)
         }
+        
         context.flush()
     }
 }
@@ -218,6 +231,9 @@ internal final class WebSocketActorMessageInboundHandler: ChannelInboundHandler 
         self.actorSystem = actorSystem
     }
     
+}
+
+extension WebSocketActorMessageInboundHandler {
     public func channelRead(context: ChannelHandlerContext, data: NIOAny) {
         let frame = self.unwrapInboundIn(data)
         
